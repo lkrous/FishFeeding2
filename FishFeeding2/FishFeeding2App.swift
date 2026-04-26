990e600a7c4ceb1454bb40dac87041c4183b8023
LeRoy Krous <lkr3991@gmail.com>
2026-04-25 19:52:07 -0500

    Initial Commit

---
+++ FishFeeding2/FishFeeding2App.swift
@@ -0,0 +1,17 @@
+//
+//  FishFeeding2App.swift
+//  FishFeeding2
+//
+//  Created by LeRoy Krous on 4/25/26.
+//
+
+import SwiftUI
+
+@main
+struct FishFeeding2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
